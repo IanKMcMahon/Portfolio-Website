e0398e2cd9afb7619a136562eb99844d019a2ad5
Your Name <you@example.com>
2025-01-08 13:05:12 -0800

    create and import Navbar component

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<h1>Ian McMahon</h1>
+    <>
+<div><Navbar/></div>
+</>
   )
 }
 
